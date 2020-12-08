5991f12a91e79ece86c2e62be597fbab2382a2ef
kjhlove55 <hilovemei928@gmail.com>
2020-12-08 21:25:57 +0900

    Update Test1.java

--- jsBasic/src/jsBasic/Test1.java
+++ jsBasic/src/jsBasic/Test1.java
@@ -3,7 +3,8 @@ package jsBasic;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Git updated");
+		System.out.println("Git updated");	
+		System.out.println("가능??");
 	}
 
 }
